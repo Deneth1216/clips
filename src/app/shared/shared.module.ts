e93504ee8cc4c326a76e8268fdb231e2d3a38313
Deneth Sathsara <info@denethsathsara.com>
2022-04-17 15:32:16 +0530

    Designing the modal Component with Logins

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,19 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModalComponent } from './components/modal/modal.component';
+import { TabsContainerComponent } from './components/tabs-container/tabs-container.component';
+import { TabComponent } from './components/tab/tab.component';
 
 
 
 
 @NgModule({
   declarations: [
-    ModalComponent
+    ModalComponent,
+    TabsContainerComponent,
+    TabComponent
   ],
   imports: [
     CommonModule
   ], 
   exports: [
-    ModalComponent
+    ModalComponent,
+    TabsContainerComponent,
+    TabComponent
   ],
 
 })
